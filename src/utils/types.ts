f8dbceb59312f37968af9ecfc8886d72717a5500
pnzenang <pnzenang@gmail.com>
2026-04-22 11:34:11 -0400

    4.22.commit4

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -59,7 +59,7 @@ export enum reasonForLeaving {
   NoReason = 'No Reason',
   Relocated = 'Moved out of USA',
   TooExpensive = 'Too Expensive',
-  Passed = 'Dead During Waiting Period'
+  Passed = 'Dead in the Waiting Period'
 }
 
 export type DeceasedMemberType = {
