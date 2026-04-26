ef92ac59bc323b09109feeed50d7f61935c240f7
pnzenang <pnzenang@gmail.com>
2026-04-25 20:33:46 -0400

    4.25.commit5

--- src/app/(dashboard)/new-additions/page.tsx
+++ src/app/(dashboard)/new-additions/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const NewAdditions = () => {
-  return <div>NewAdditions</div>
+  return (
+    <div>
+      <iframe src='https://docs.google.com/spreadsheets/d/e/2PACX-1vQT8xQUCPYKSCCB9ggChdonRC76QEN1p0dFCQqiEO_Ox5Afo7OrVMiiVZfjkV5Pr7jSFcpv9BjE0nSx/pubhtml?widget=true&amp;headers=false'></iframe>
+    </div>
+  )
 }
 
 export default NewAdditions
