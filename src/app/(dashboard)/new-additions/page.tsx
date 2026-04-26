8723df0be195c3582b67126387ab8fc3ce9afa91
pnzenang <pnzenang@gmail.com>
2026-04-25 20:38:54 -0400

    4.25.commit6

--- src/app/(dashboard)/new-additions/page.tsx
+++ src/app/(dashboard)/new-additions/page.tsx
@@ -1,9 +1,19 @@
+import { Card } from '@/components/ui/card'
 import React from 'react'
 
 const NewAdditions = () => {
   return (
     <div>
-      <iframe src='https://docs.google.com/spreadsheets/d/e/2PACX-1vQT8xQUCPYKSCCB9ggChdonRC76QEN1p0dFCQqiEO_Ox5Afo7OrVMiiVZfjkV5Pr7jSFcpv9BjE0nSx/pubhtml?widget=true&amp;headers=false'></iframe>
+      <Card className='mx-auto my-2 max-w-7xl rounded-lg border bg-white p-4'>
+        <h1 className='text-muted-foreground py-1 text-sm font-bold sm:text-3xl lg:text-5xl'>CONTRIBUTIONS RECAP</h1>
+        <h1 className='text-muted-foreground text-sm font-bold sm:text-lg'>
+          In this section, you can find The list of monthly newly registered from all the loved ones.
+        </h1>
+      </Card>
+      <iframe
+        src='https://docs.google.com/spreadsheets/d/e/2PACX-1vQT8xQUCPYKSCCB9ggChdonRC76QEN1p0dFCQqiEO_Ox5Afo7OrVMiiVZfjkV5Pr7jSFcpv9BjE0nSx/pubhtml?widget=true&amp;headers=false'
+        className='max-w-9xl mx-auto h-160 w-full items-center'
+      ></iframe>
     </div>
   )
 }
