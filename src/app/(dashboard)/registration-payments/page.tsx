b13296880dcc28a6e3d99d9270450d442bf4c862
pnzenang <pnzenang@gmail.com>
2026-04-26 23:22:54 -0400

    4.26.commit16

--- src/app/(dashboard)/registration-payments/page.tsx
+++ src/app/(dashboard)/registration-payments/page.tsx
@@ -8,10 +8,12 @@ const RegistrationPayments = () => {
   // const user = await fetchProfile()
   return (
     <section>
-      <FormfacadeEmbed
-        formFacadeURL='https://formfacade.com/include/112423225580039142072/form/1FAIpQLSci8U_8cWIuWoiWob6Hx1-WH2gBfelrTVWGnhK8BCn9FH5OkA/classic.js/?div=ff-compose'
-        onSubmitForm={() => console.log('Form submitted')}
-      />
+      <div className='mx-auto mt-5 w-full max-w-19/20 rounded-lg'>
+        <FormfacadeEmbed
+          formFacadeURL='https://formfacade.com/include/112423225580039142072/form/1FAIpQLSci8U_8cWIuWoiWob6Hx1-WH2gBfelrTVWGnhK8BCn9FH5OkA/classic.js/?div=ff-compose'
+          onSubmitForm={() => console.log('Form submitted')}
+        />
+      </div>
       <iframe
         src='https://docs.google.com/spreadsheets/d/e/2PACX-1vSJLcE1s8Z0nKXZB065VKAYS6CguaFvZFYfUOICSzEcAP6VEnXsZbHC1fft30X1WkAcM2TWJGuSscb1/pubhtml?widget=true&amp;headers=false'
         className='mx-auto mt-5 h-110 w-full max-w-19/20 rounded-lg border'
