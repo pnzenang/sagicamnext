b687d44bd6b5b4c0fff9205b89eb4108bfc1c83c
pnzenang <pnzenang@gmail.com>
2026-04-26 23:14:37 -0400

    4.26.commit15

--- src/app/(dashboard)/registration-payments/page.tsx
+++ src/app/(dashboard)/registration-payments/page.tsx
@@ -14,7 +14,7 @@ const RegistrationPayments = () => {
       />
       <iframe
         src='https://docs.google.com/spreadsheets/d/e/2PACX-1vSJLcE1s8Z0nKXZB065VKAYS6CguaFvZFYfUOICSzEcAP6VEnXsZbHC1fft30X1WkAcM2TWJGuSscb1/pubhtml?widget=true&amp;headers=false'
-        className='mx-auto mt-5 h-90 w-full max-w-18/20 rounded-lg border'
+        className='mx-auto mt-5 h-110 w-full max-w-19/20 rounded-lg border'
       ></iframe>
     </section>
   )
