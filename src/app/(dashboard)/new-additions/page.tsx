1579aaa73d73e82dc5575c8cb5824b6f94350103
pnzenang <pnzenang@gmail.com>
2026-04-27 00:07:41 -0400

    4.27.1.commit1

--- src/app/(dashboard)/new-additions/page.tsx
+++ src/app/(dashboard)/new-additions/page.tsx
@@ -5,7 +5,9 @@ const NewAdditions = () => {
   return (
     <div>
       <Card className='mx-auto my-2 max-w-7xl rounded-lg border bg-white p-4'>
-        <h1 className='text-muted-foreground py-1 text-sm font-bold sm:text-3xl lg:text-5xl'>CONTRIBUTIONS RECAP</h1>
+        <h1 className='text-muted-foreground py-1 text-sm font-bold sm:text-3xl lg:text-5xl'>
+          NEWLY VESTED LOVED ONES
+        </h1>
         <h1 className='text-muted-foreground text-sm font-bold sm:text-lg'>
           In this section, you can find The list of monthly newly registered from all the sponsors.
         </h1>
