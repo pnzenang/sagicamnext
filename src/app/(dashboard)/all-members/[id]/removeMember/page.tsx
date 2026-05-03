ca749c3dccc18258784faa565a7092d19823303c
pnzenang <pnzenang@gmail.com>
2026-05-03 17:17:55 -0400

    5.3.commit5

--- src/app/(dashboard)/all-members/[id]/removeMember/page.tsx
+++ src/app/(dashboard)/all-members/[id]/removeMember/page.tsx
@@ -79,8 +79,8 @@ const RemoveMember = async ({ params }: { params: { id: string } }) => {
               <div className='mt-10 flex flex-col items-center justify-center gap-1 sm:flex-row'>
                 <BsSignStopFill className='size-8 items-center text-red-500' />{' '}
                 <h1 className='text-center text-sm font-semibold text-red-500 sm:text-lg'>
-                  SAGI prevents withdrawal between the 6th and the 25th of the month, in order to ensure accuracy of the
-                  contribution.
+                  SAGICAM prevents withdrawal between the 6th and the 25th of the month, in order to ensure accuracy of
+                  the contribution.
                 </h1>
               </div>
             )}
