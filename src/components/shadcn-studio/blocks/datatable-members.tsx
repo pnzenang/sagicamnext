568335eba4c59c4c43c05cbf160e7eacfcf61bd6
pnzenang <pnzenang@gmail.com>
2026-05-03 17:23:08 -0400

    5.3.commit7

--- src/components/shadcn-studio/blocks/datatable-members.tsx
+++ src/components/shadcn-studio/blocks/datatable-members.tsx
@@ -604,16 +604,14 @@ function RowActions({ memberId }: { memberId: string }) {
               </span>
             </Link>
           </DropdownMenuItem>
-          {shouldShow ? (
-            <DropdownMenuItem>
-              <Link href={`/all-members/${memberId}/removeMember`}>
-                <span className='flex flex-row gap-3 text-red-500'>
-                  <Trash2 className='text-red-500' />
-                  Remove Member
-                </span>
-              </Link>
-            </DropdownMenuItem>
-          ) : null}
+          <DropdownMenuItem>
+            <Link href={`/all-members/${memberId}/removeMember`}>
+              <span className='flex flex-row gap-3 text-red-500'>
+                <Trash2 className='text-red-500' />
+                Remove Member
+              </span>
+            </Link>
+          </DropdownMenuItem>
         </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
